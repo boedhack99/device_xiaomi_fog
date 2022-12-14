eb4bc8d86d905aca638bf2f72eaa5c89cece42a5
boedhack99 <boedhack@gmail.com>
2022-12-14 17:02:00 +0700

    fog: ArrowOS bringup
    Change-Id: I438774bb1773f25a16364b2b03b8aaabbfc5ef27

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_fog.mk
+    $(LOCAL_DIR)/arrow_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_fog-user \
-    nad_fog-userdebug \
-    nad_fog-eng
+    arrow_fog-user \
+    arrow_fog-userdebug \
+    arrow_fog-eng
