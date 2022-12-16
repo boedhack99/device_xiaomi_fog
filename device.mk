1b1018bf4e3dcdf0d0cf282e3447d4c1e2a05d05
boedhack99 <boedhack@gmail.com>
2022-12-16 21:00:57 +0700

    fog: vibrator: Switch to build source vibrator service
    Change-Id: Iee10c4b6c0391486b54eca634c42df65698052fb

--- device.mk
+++ device.mk
@@ -479,7 +479,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.fog
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
