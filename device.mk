9920328c95a735903bdeda1d97c46131abceb694
boedhack99 <boedhack@gmail.com>
2022-12-29 00:56:26 +0700

    Revert "fog: vibrator: Call vibrator-vendor-product.mk"
    This reverts commit ac05230ac61bf40b0132edf178a3d8acb15a7c8c.

--- device.mk
+++ device.mk
@@ -458,7 +458,11 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
